2b845047b45de8460d414e3e83f4b5cfee4cfd64
sujalkakadiya34 <sujalkakadiya1296@gmail.com>
2025-08-27 18:11:15 +0530

    Initial react setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>iwetechs</h1>
+        <h2>At RK SUPERME</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
